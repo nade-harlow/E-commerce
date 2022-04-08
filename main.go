74c5b3685968a4643f41312f29738e739be723de
nade-harlow <omonadefranklyn@gmail.com>
2022-04-08 19:20:50 +0100

    logrus initialized

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"github.com/joho/godotenv"
 	"github.com/nade-harlow/E-commerce/adapter/api/server"
+	"github.com/sirupsen/logrus"
 	"log"
 	"os"
 )
 
 func main() {
+	logger := logrus.New()
+	logger.Formatter = &logrus.JSONFormatter{PrettyPrint: true}
+	log.SetOutput(logger.Writer())
+
 	err := godotenv.Load()
 	if err != nil && os.Getenv("ENV") != "dev" {
 		log.Fatal("Error loading .env file!")
