6cb00281e8422bd9291ca0606b4699091de4735d
nade-harlow <omonadefranklyn@gmail.com>
2022-04-08 14:34:44 +0100

    product route

--- adapter/api/routes/routes.go
+++ adapter/api/routes/routes.go
@@ -1,9 +1,13 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nade-harlow/E-commerce/adapter/api/controllers/product"
+)
 
 func DefineRoutes(r *gin.Engine) {
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, map[string]string{"message": "Hello World!"})
 	})
+	r.GET("/products", product.GetProduct())
 }
