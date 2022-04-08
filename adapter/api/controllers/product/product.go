167aa6bda3e9535c667bedb26e8a82a5b5fd2e73
nade-harlow <omonadefranklyn@gmail.com>
2022-04-08 14:31:45 +0100

    product controller

---
+++ adapter/api/controllers/product/product.go
@@ -0,0 +1,11 @@
+package product
+
+import "github.com/gin-gonic/gin"
+
+func GetProduct() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	}
+}
