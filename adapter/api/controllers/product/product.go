1ba8752bf7e455d22c46e84015db409c500136e3
nade-harlow <omonadefranklyn@gmail.com>
2022-04-08 16:28:46 +0100

    product handler

--- adapter/api/controllers/product/product.go
+++ adapter/api/controllers/product/product.go
@@ -1,11 +1,31 @@
 package product
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nade-harlow/E-commerce/core/models"
+	"github.com/nade-harlow/E-commerce/ports"
+	"log"
+)
 
-func GetProduct() gin.HandlerFunc {
+type ProductController struct {
+	ProductService ports.Service
+	route          *gin.Engine
+}
+
+func NewProductController(productService ports.Service) *ProductController {
+	return &ProductController{
+		ProductService: productService,
+	}
+}
+
+func (products *ProductController) GetProduct() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+		product := models.Product{}
+		c.ShouldBindJSON(&product)
+		err := products.ProductService.Create(&product)
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
 	}
 }
