595ea4a9b1d6e08bebea21b7e9090197daa7b661
nade-harlow <omonadefranklyn@gmail.com>
2022-04-09 15:01:44 +0100

    before save method added

--- core/models/model.go
+++ core/models/model.go
@@ -1,13 +1,19 @@
 package models
 
 import (
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"time"
 )
 
 type Model struct {
-	ID        int            `gorm:"primaryKey" json:"id"`
+	ID        string         `gorm:"primaryKey" json:"id"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-" `
+}
+
+func (u *Model) BeforeCreate(tx *gorm.DB) {
+	u.ID = uuid.New().String()
+	return
 }
