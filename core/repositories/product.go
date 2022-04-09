dc2833a76b76ee77427199883d11dd0cad8a6ccd
nade-harlow <omonadefranklyn@gmail.com>
2022-04-09 16:08:04 +0100

    receiver refactored

--- core/repositories/product.go
+++ core/repositories/product.go
@@ -4,8 +4,8 @@ import (
 	"github.com/nade-harlow/E-commerce/core/models"
 )
 
-func (p *Repository) CreateProduct(product *models.Product) error {
-	if tx := p.DB.Create(product); tx.Error != nil {
+func (repo *Repository) CreateProduct(product *models.Product) error {
+	if tx := repo.DB.Create(product); tx.Error != nil {
 		return tx.Error
 	}
 	return nil
