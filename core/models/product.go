07cabe58a2f492d865ec24c5ba207aa4946a8812
nade-harlow <omonadefranklyn@gmail.com>
2022-04-09 15:02:01 +0100

    before save method added

--- core/models/product.go
+++ core/models/product.go
@@ -1,8 +1,22 @@
 package models
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"time"
+)
+
 type Product struct {
-	Id       int64  `json:"id"`
-	Name     string `json:"name"`
-	Price    int64  `json:"price"`
-	Quantity int64  `json:"quantity"`
+	Id        string         `gorm:"primaryKey" json:"id"`
+	Name      string         `json:"name"`
+	Price     int64          `json:"price"`
+	Quantity  int64          `json:"quantity"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-" `
+}
+
+func (p *Product) BeforeCreate(tx *gorm.DB) (err error) {
+	p.Id = uuid.New().String()
+	return
 }
