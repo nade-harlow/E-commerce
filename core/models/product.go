4f507f7c8128efc07c088988feb022d64d562ae3
nade-harlow <omonadefranklyn@gmail.com>
2022-04-09 15:22:14 +0100

    user struct refactored

--- core/models/product.go
+++ core/models/product.go
@@ -1,22 +1,16 @@
 package models
 
 import (
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"time"
 )
 
 type Product struct {
-	ID        string         `gorm:"primaryKey" json:"id"`
+	Model
 	Name      string         `gorm:"unique" json:"name"`
 	Price     int64          `json:"price"`
 	Quantity  int64          `json:"quantity"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-" `
-}
-
-func (p *Product) BeforeCreate(tx *gorm.DB) (err error) {
-	p.ID = uuid.New().String()
-	return
 }
