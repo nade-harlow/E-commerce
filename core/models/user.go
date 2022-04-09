abe55ce819b05587f2be75a9d561417275f14b73
nade-harlow <omonadefranklyn@gmail.com>
2022-04-09 15:19:49 +0100

    user fields added

--- core/models/user.go
+++ core/models/user.go
@@ -1,7 +1,27 @@
 package models
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"time"
+)
+
 type User struct {
-	Model
-	Username string `json:"username"`
-	Password string `json:"password"`
+	ID          string         `gorm:"primaryKey" json:"id"`
+	FirstName   string         `json:"first_name"`
+	LastName    string         `json:"last_name"`
+	Username    string         `json:"username"`
+	Country     string         `json:"country"`
+	DateOfBirth string         `json:"dateofbirth"`
+	Avatar      string         `json:"avatar"`
+	Email       string         `gorm:"unique" json:"email"`
+	Password    string         `json:"-"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-" `
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	u.ID = uuid.New().String()
+	return
 }
