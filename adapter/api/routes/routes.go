4b1c81550ec805928f00cb7352ad8501736e8255
nade-harlow <omonadefranklyn@gmail.com>
2022-04-15 17:10:07 +0100

    user routes added

--- adapter/api/routes/routes.go
+++ adapter/api/routes/routes.go
@@ -3,11 +3,16 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/nade-harlow/E-commerce/adapter/api/controllers/product"
+	"github.com/nade-harlow/E-commerce/adapter/api/controllers/user"
 )
 
-func DefineRoutes(r *gin.Engine, pc *product.ProductController) {
+func ProductRoutes(r *gin.Engine, pc *product.ProductController) {
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, map[string]string{"message": "Hello World!"})
 	})
 	r.POST("/products", pc.AddProduct())
+}
+
+func UserRoutes(r *gin.Engine, uc *user.UserController) {
+	r.POST("/users")
 }
