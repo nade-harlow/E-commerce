c694d8207f1c557ae9cd5649279e9ad8fb1fc3a9
nade-harlow <omonadefranklyn@gmail.com>
2022-04-16 10:50:22 +0100

    sign up user handler refactored

--- adapter/api/controllers/user/user.go
+++ adapter/api/controllers/user/user.go
@@ -2,6 +2,9 @@ package user
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/nade-harlow/E-commerce/core/models"
+	"github.com/nade-harlow/E-commerce/core/utils"
+	"github.com/nade-harlow/E-commerce/core/utils/response"
 	"github.com/nade-harlow/E-commerce/ports/services"
 )
 
@@ -13,5 +16,34 @@ type UserController struct {
 func NewUserController(productService services.UserServices) *UserController {
 	return &UserController{
 		UserService: productService,
+	}
+}
+
+func (user *UserController) SignUpUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var userRequest models.User
+		if err := c.ShouldBindJSON(&userRequest); err != nil {
+			response.Json(c, 500, "Error binding json", nil, err.Error())
+			return
+		}
+		if err := utils.ValidateStruct(userRequest); err != nil {
+			response.Json(c, 400, "Error validating data", nil, err.Error())
+			return
+		}
+		if err := user.UserService.SignUpUser(&userRequest); err != nil {
+			response.Json(c, 500, "Error creating user", nil, err.Error())
+			return
+		}
+		token, err := utils.GenerateToken(&userRequest)
+		if err != nil {
+			response.Json(c, 500, "Error generating token", nil, err.Error())
+			return
+		}
+		data := map[string]interface{}{
+			"token":           token,
+			"user_id":         userRequest.ID,
+			"user_first_name": userRequest.FirstName,
+		}
+		response.Json(c, 200, "User created", data, nil)
 	}
 }
