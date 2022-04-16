d0bc1cd0ffae483f31ad4bcec05a0eb0f58e595f
nade-harlow <omonadefranklyn@gmail.com>
2022-04-16 10:48:38 +0100

    user model remodeled

--- core/models/user.go
+++ core/models/user.go
@@ -7,17 +7,48 @@ import (
 
 type User struct {
 	Model
-	FirstName   string         `json:"first_name"`
-	LastName    string         `json:"last_name"`
-	Username    string         `json:"username"`
-	Country     string         `json:"country"`
-	DateOfBirth string         `json:"dateofbirth"`
-	Avatar      string         `json:"avatar"`
-	Email       string         `gorm:"unique" json:"email"`
-	Password    string         `json:"password,omitempty"`
-	CartID      string         `json:"cart_id"`
-	Cart        Cart           `json:"cart"`
-	CreatedAt   time.Time      `json:"created_at"`
-	UpdatedAt   time.Time      `json:"updated_at"`
-	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-" `
+	FirstName string         `json:"first_name"`
+	LastName  string         `json:"last_name"`
+	Username  string         `json:"username"`
+	Role      string         `json:"role"`
+	Email     string         `gorm:"unique" json:"email"`
+	Password  string         `json:"password,omitempty"`
+	Telephone string         `json:"telephone"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-" `
+}
+
+type UserAddress struct {
+	Model
+	UserID       string         `json:"user_id"`
+	User         User           `json:"user"`
+	AddressLine1 string         `json:"address_line_1"`
+	AddressLine2 string         `json:"address_line_2"`
+	City         string         `json:"city"`
+	PostalCode   string         `json:"postal_code"`
+	Country      string         `json:"country"`
+	Telephone    string         `json:"telephone"`
+	Mobile       string         `json:"mobile"`
+	CreatedAt    time.Time      `json:"created_at"`
+	UpdatedAt    time.Time      `json:"updated_at"`
+	DeletedAt    gorm.DeletedAt `gorm:"index" json:"-" `
+}
+
+type UserPayment struct {
+	Model
+	UserID     string         `json:"user_id"`
+	User       User           `json:"user"`
+	CardNumber string         `json:"card_number"`
+	CardName   string         `json:"card_name"`
+	CardExpiry string         `json:"card_expiry"`
+	CardCvv    string         `json:"card_cvv"`
+	CreatedAt  time.Time      `json:"created_at"`
+	UpdatedAt  time.Time      `json:"updated_at"`
+	DeletedAt  gorm.DeletedAt `gorm:"index" json:"-" `
+}
+
+type Role struct {
+	Model
+	Role string `json:"name"`
 }
