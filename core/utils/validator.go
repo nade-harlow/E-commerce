7f44b99eb1430bc1263fe16f8a54dcec489ca458
nade-harlow <omonadefranklyn@gmail.com>
2022-04-16 10:49:05 +0100

    validator added

---
+++ core/utils/validator.go
@@ -0,0 +1,60 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+)
+
+var validate *validator.Validate
+
+func ValidateStruct(data interface{}) error {
+
+	validate = validator.New()
+	// returns nil or ValidationErrors ( []FieldError )
+	err := validate.Struct(data)
+	if err != nil {
+
+		// this check is only needed when your code could produce
+		// an invalid value for validation such as interface with nil
+		// value most including myself do not usually have code like this.
+		if _, ok := err.(*validator.InvalidValidationError); ok {
+			fmt.Println(err)
+			return err
+		}
+
+		for _, err := range err.(validator.ValidationErrors) {
+
+			fmt.Println(err.Namespace())
+			fmt.Println(err.Field())
+			fmt.Println(err.StructNamespace())
+			fmt.Println(err.StructField())
+			fmt.Println(err.Tag())
+			fmt.Println(err.ActualTag())
+			fmt.Println(err.Kind())
+			fmt.Println(err.Type())
+			fmt.Println(err.Value())
+			fmt.Println(err.Param())
+			fmt.Println()
+		}
+
+		// from here you can create your own error messages in whatever language you wish
+		return err
+	}
+
+	// save user to database
+	return nil
+}
+
+func validateVariable() {
+
+	myEmail := "joeybloggs.gmail.com"
+
+	errs := validate.Var(myEmail, "required,email")
+
+	if errs != nil {
+		fmt.Println(errs) // output: Key: "" Error:Field validation for "" failed on the "email" tag
+		return
+	}
+
+	// email ok, move on
+}
