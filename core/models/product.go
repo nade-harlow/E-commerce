74531ecaa3606051a287ff047e2648034fed999b
nade-harlow <omonadefranklyn@gmail.com>
2022-04-16 10:48:04 +0100

    product model remodeled

--- core/models/product.go
+++ core/models/product.go
@@ -7,11 +7,44 @@ import (
 
 type Product struct {
 	Model
-	Name        string         ` json:"name"`
-	Description string         ` json:"description"`
-	Price       int64          `json:"price"`
-	Quantity    int64          `json:"quantity"`
+	Name              string          ` json:"name"`
+	Description       string          ` json:"description"`
+	Sku               string          ` json:"sku"`
+	ProductCategoryID string          ` json:"category_id"`
+	ProductCategory   ProductCategory ` json:"category"`
+	Price             float32         `json:"price"`
+	Quantity          int16           `json:"quantity"`
+	CreatedAt         time.Time       `json:"created_at"`
+	UpdatedAt         time.Time       `json:"updated_at"`
+	DeletedAt         gorm.DeletedAt  `gorm:"index" json:"-" `
+}
+
+type ProductCategory struct {
+	Model
+	Name        string         `json:"name"`
+	Description string         `json:"description"`
 	CreatedAt   time.Time      `json:"created_at"`
 	UpdatedAt   time.Time      `json:"updated_at"`
 	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-" `
 }
+
+//type ProductInventory struct {
+//	Model
+//	ProductID string         `json:"product_id"`
+//	Product   *Product       `json:"product"`
+//	Quantity  int16          `json:"quantity"`
+//	CreatedAt time.Time      `json:"created_at"`
+//	UpdatedAt time.Time      `json:"updated_at"`
+//	DeletedAt gorm.DeletedAt `gorm:"index" json:"-" `
+//}
+
+//type ProductDiscount struct {
+//	Model
+//	Name               string         `json:"name"`
+//	Description        string         `json:"description"`
+//	DiscountPercentage float32        `json:"discount_percentage"`
+//	Active             bool           `json:"active"`
+//	CreatedAt          time.Time      `json:"created_at"`
+//	UpdatedAt          time.Time      `json:"updated_at"`
+//	DeletedAt          gorm.DeletedAt `gorm:"index" json:"-" `
+//}
