e32010ce32e9a4bf2d6ac506d75f9caf772c3a79
nade-harlow <omonadefranklyn@gmail.com>
2022-04-16 10:47:45 +0100

    cart model remodeled

--- core/models/cart.go
+++ core/models/cart.go
@@ -1,16 +1,55 @@
 package models
 
 import (
-	"gorm.io/gorm"
 	"time"
 )
 
-type Cart struct {
+type CartItem struct {
 	Model
-	ProductID string         `json:"product_id"`
-	Product   Product        `json:"products"`
-	Quantity  int            `json:"quantity"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-" `
+	ShoppingSessionID string          `json:"shopping_session_id"`
+	ShoppingSession   ShoppingSession `json:"shopping_session"`
+	ProductID         string          `json:"product_id"`
+	Product           Product         `json:"products"`
+	Quantity          int16           `json:"quantity"`
+	CreatedAt         time.Time       `json:"created_at"`
+	UpdatedAt         time.Time       `json:"updated_at"`
+}
+
+type ShoppingSession struct {
+	Model
+	UserID    string    `json:"user_id"`
+	User      User      `json:"user"`
+	Total     float32   `json:"total"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type OrderItems struct {
+	Model
+	OrderDetailsID string       `json:"order_details_id"`
+	OrderDetails   OrderDetails `json:"order_details"`
+	ProductID      string       `json:"product_id"`
+	Product        Product      `json:"products"`
+	Quantity       int16        `json:"quantity"`
+	CreatedAt      time.Time    `json:"created_at"`
+	UpdatedAt      time.Time    `json:"updated_at"`
+}
+
+type OrderDetails struct {
+	Model
+	UserID    string    `json:"user_id"`
+	User      User      `json:"user"`
+	Total     float32   `json:"total"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type PaymentDetails struct {
+	Model
+	OrderDetailsID string       `json:"order_details_id"`
+	OrderDetails   OrderDetails `json:"order_details"`
+	PaymentMethod  string       `json:"payment_method"`
+	PaymentStatus  string       `json:"payment_status"`
+	CreatedAt      time.Time    `json:"created_at"`
+	UpdatedAt      time.Time    `json:"updated_at"`
 }
