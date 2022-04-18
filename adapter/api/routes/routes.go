c82c82bd2de60e4a70cdae6616dd2a5afaa5bf64
nade-harlow <omonadefranklyn@gmail.com>
2022-04-18 22:30:58 +0100

    middleware added to route

--- adapter/api/routes/routes.go
+++ adapter/api/routes/routes.go
@@ -4,13 +4,14 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/nade-harlow/E-commerce/adapter/api/controllers/product"
 	"github.com/nade-harlow/E-commerce/adapter/api/controllers/user"
+	"github.com/nade-harlow/E-commerce/adapter/api/middleware"
 )
 
 func ProductRoutes(r *gin.Engine, pc *product.ProductController) {
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, map[string]string{"message": "Hello World!"})
 	})
-	r.POST("/products", pc.AddProduct())
+	r.POST("/products", middleware.AuthorizeToken(), pc.AddProduct())
 }
 
 func UserRoutes(r *gin.Engine, uc *user.UserController) {
