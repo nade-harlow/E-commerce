c995fc620c2252fe4418f8323df69e4e216e4f51
nade-harlow <omonadefranklyn@gmail.com>
2022-05-04 03:13:25 +0100

    template parser added

---
+++ core/utils/fuctions.go
@@ -0,0 +1,10 @@
+package utils
+
+//func parseTemplate(myData accountmodel.RecoverPasswordVM) string {
+//
+//	tpl, _ := ioutil.ReadFile("core/template/validate_otp.html")
+//
+//	output := raymond.MustRender(string(tpl), myData)
+//
+//	return output
+//}
