da4e633d32988f8dc746829c3f1fc0def172c603
nade-harlow <omonadefranklyn@gmail.com>
2022-05-05 19:45:47 +0100

    added template render function

--- core/utils/fuctions.go
+++ core/utils/fuctions.go
@@ -1,10 +1,13 @@
 package utils
 
-//func parseTemplate(myData accountmodel.RecoverPasswordVM) string {
-//
-//	tpl, _ := ioutil.ReadFile("core/template/verify_email.html")
-//
-//	output := raymond.MustRender(string(tpl), myData)
-//
-//	return output
-//}
+import (
+	"github.com/aymerick/raymond"
+	"github.com/nade-harlow/E-commerce/core/models"
+	"io/ioutil"
+)
+
+func ParseTemplate(Data models.RecoverPassword) string {
+	template, _ := ioutil.ReadFile("core/template/reset_password.html")
+	body := raymond.MustRender(string(template), Data)
+	return body
+}
