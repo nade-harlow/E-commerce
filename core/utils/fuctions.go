b296fb6a814d72f03ac760d151c43ea485fe2174
nade-harlow <omonadefranklyn@gmail.com>
2022-05-05 15:16:46 +0100

    [Refactor] file path updated

--- core/utils/fuctions.go
+++ core/utils/fuctions.go
@@ -2,7 +2,7 @@ package utils
 
 //func parseTemplate(myData accountmodel.RecoverPasswordVM) string {
 //
-//	tpl, _ := ioutil.ReadFile("core/template/validate_otp.html")
+//	tpl, _ := ioutil.ReadFile("core/template/verify_email.html")
 //
 //	output := raymond.MustRender(string(tpl), myData)
 //
