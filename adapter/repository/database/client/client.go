4666f1013fbf5da0ec1507bc775af145f7aa1e3a
nade-harlow <omonadefranklyn@gmail.com>
2022-05-05 21:48:20 +0100

    removed shopping session

--- adapter/repository/database/client/client.go
+++ adapter/repository/database/client/client.go
@@ -20,8 +20,8 @@ func InitializeConnection() *gorm.DB {
 		log.Fatal("Failed to connect to postgresql database")
 	}
 	err = postgresql.SetupDatabase(db, &models.User{}, &models.UserAddress{}, &models.UserPayment{}, &models.UserPayment{},
-		&models.Product{}, &models.ProductCategory{}, &models.CartItem{}, &models.ProductImage{},
-		&models.ShoppingSession{}, &models.OrderDetails{}, &models.OrderItems{}, &models.PaymentDetails{}, &models.PasswordRecovery{})
+		&models.Product{}, &models.ProductCategory{}, &models.CartItem{}, &models.ProductImage{}, &models.OrderDetails{},
+		&models.OrderItems{}, &models.PaymentDetails{}, &models.PasswordRecovery{})
 	if err != nil {
 		log.Fatal("Failed to setup database")
 	}
