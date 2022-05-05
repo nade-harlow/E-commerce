fbc553552b3a3c25676684bb1e45cb04c7b5fd75
nade-harlow <omonadefranklyn@gmail.com>
2022-05-05 19:49:04 +0100

    added reset password service

--- ports/services/user.go
+++ ports/services/user.go
@@ -9,6 +9,7 @@ import (
 	"github.com/nade-harlow/E-commerce/core/utils"
 	repository2 "github.com/nade-harlow/E-commerce/ports/repositories"
 	"log"
+	"os"
 	"time"
 )
 
@@ -21,14 +22,18 @@ type UserServices interface {
 	VerifyUser(code string) error
 	AddUserAddress(address *models.UserAddress) error
 	UpdateUserAddress(user *models.UserAddress) error
+	ForgotPassword(userID, email string) error
 	ResetUserPassword(userID string, password string) error
 }
 
 type UserService struct {
-	repository repository2.UserRepository
+	repository   repository2.UserRepository
+	notification notification.MailgunRepository
 }
 
 const SmsOtpMessage = "Please use the OTP Code: %s to complete your registration. Code expires in five minutes"
+
+var BaseURL = os.Getenv("BASE_URL")
 
 func NewUserService(repository repository2.UserRepository) UserServices {
 	return &UserService{
@@ -88,4 +93,14 @@ func (user *UserService) UpdateUserAddress(address *models.UserAddress) error {
 
 func (user *UserService) ResetUserPassword(userID string, password string) error {
 	return user.repository.ResetUserPassword(userID, password)
+}
+
+func (user UserService) ForgotPassword(userID, email string) error {
+	subject := "E-commerce: Reset Password"
+	data := models.RecoverPassword{
+		BaseUrl: os.Getenv("BASE_URL"),
+		ID:      userID,
+	}
+	body := utils.ParseTemplate(data)
+	return user.notification.SendMail(email, subject, body)
 }
