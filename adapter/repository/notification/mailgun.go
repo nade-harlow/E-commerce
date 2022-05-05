06e4debba34bbc15bdaea7511b1746c3b7184c99
nade-harlow <omonadefranklyn@gmail.com>
2022-05-05 19:47:14 +0100

    added send mail method using mailgun

--- adapter/repository/notification/mailgun.go
+++ adapter/repository/notification/mailgun.go
@@ -2,21 +2,31 @@ package notification
 
 import (
 	"context"
+	"fmt"
 	"github.com/mailgun/mailgun-go/v4"
+	"log"
 	"os"
 	"time"
 )
 
-var domain = os.Getenv("MG_DOMAIN")
-var apiKey = os.Getenv("MG_PUBLIC_API_KEY")
+// MailgunRepository is a repository for sending emails
+type MailgunRepository struct {
+	client *mailgun.MailgunImpl
+}
 
-func AddTemplate(domain, apiKey string) error {
-	mg := mailgun.NewMailgun(domain, apiKey)
+// NewMailgunRepository creates a new MailgunRepository
+func (mg *MailgunRepository) NewMailgunRepository() {
+	fmt.Println(os.Getenv("MG_DOMAIN"))
+	mg.client = mailgun.NewMailgun(os.Getenv("MG_DOMAIN"), os.Getenv("MG_API_KEY"))
+	//fmt.Println("domooo", mg.client.Domain())
+}
+
+func (mg *MailgunRepository) AddTemplate() error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
 	defer cancel()
 
-	return mg.CreateTemplate(ctx, &mailgun.Template{
-		Name: "verify.account",
+	return mg.client.CreateTemplate(ctx, &mailgun.Template{
+		Name: "**template name**",
 		Version: mailgun.TemplateVersion{
 			Template: ``,
 			Engine:   mailgun.TemplateEngineGo,
@@ -25,30 +35,29 @@ func AddTemplate(domain, apiKey string) error {
 	})
 }
 
-func SendMessageWithTemplate(email, subject string) (string, error) {
-	mg := mailgun.NewMailgun(domain, apiKey)
+func (mg *MailgunRepository) SendMessageWithTemplate(email, subject string) (string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
 	defer cancel()
 
-	m := mg.NewMessage(os.Getenv("MG_EMAIL_FROM"), subject, "")
-	m.SetTemplate("verify.account")
+	m := mg.client.NewMessage(os.Getenv("MG_EMAIL_FROM"), subject, "")
+	m.SetTemplate("**template name**")
 	if err := m.AddRecipient(email); err != nil {
 		return "", err
 	}
 
-	//m.AddVariable("link", "www.instagram.com")
-
-	mes, _, err := mg.Send(ctx, m)
+	mes, _, err := mg.client.Send(ctx, m)
 	return mes, err
 }
 
-func SendSimpleMessage(email, subject, body string) (string, error) {
-	mg := mailgun.NewMailgun(domain, apiKey)
-	m := mg.NewMessage(os.Getenv("MG_EMAIL_FROM"), subject, body, email)
+func (mg *MailgunRepository) SendMail(email, subject, body string) error {
+	mg.client = mailgun.NewMailgun(os.Getenv("MG_DOMAIN"), os.Getenv("MG_API_KEY"))
 
+	m := mg.client.NewMessage(os.Getenv("MG_EMAIL_FROM"), subject, body, email)
+	m.SetHtml(body)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
 	defer cancel()
+	_, id, err := mg.client.Send(ctx, m)
 
-	_, id, err := mg.Send(ctx, m)
-	return id, err
+	log.Println("Message ID: ", id)
+	return err
 }
