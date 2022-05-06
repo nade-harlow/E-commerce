13ce7fbdfd8c0c8ae629f2a93fc2b6e7a381f161
nade-harlow <omonadefranklyn@gmail.com>
2022-05-06 17:51:57 +0100

    remove item refactored

--- core/repositories/cart/cart.go
+++ core/repositories/cart/cart.go
@@ -34,10 +34,28 @@ func (repo CartRepository) AddItem(item models.CartItem) error {
 	return nil
 }
 
-func (repo CartRepository) RemoveItem(userID, ItemID string) error {
-	return repo.DB.Where("user_id = ? AND id = ?", userID, ItemID).Delete(&models.CartItem{}).Error
+func (repo CartRepository) RemoveItem(userID, itemID string) error {
+	var cartItem models.CartItem
+	err := repo.DB.Where("user_id = ? AND product_id = ?", userID, itemID).First(&cartItem).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return errors.New("item not found in cart")
+		}
+	} else {
+		return repo.DB.Where("user_id = ? AND id = ?", userID, itemID).Delete(&models.CartItem{}).Error
+	}
+	return nil
 }
 
-func (repo CartRepository) UpdateItem(userId string, itemId string, quantity int16) error {
-	return repo.DB.Model(&models.CartItem{}).Where("user_id = ? AND id = ?", userId, itemId).Update("quantity", quantity).Error
+func (repo CartRepository) UpdateItem(userID string, itemID string, quantity int16) error {
+	var cartItem models.CartItem
+	err := repo.DB.Where("user_id = ? AND product_id = ?", userID, itemID).First(&cartItem).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return errors.New("item not found in cart")
+		}
+	} else {
+		return repo.DB.Model(&models.CartItem{}).Where("user_id = ? AND id = ?", userID, itemID).Update("quantity", quantity).Error
+	}
+	return nil
 }
