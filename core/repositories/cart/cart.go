0f4c829bf96fb0d15e9625150cab677824b5ae72
nade-harlow <omonadefranklyn@gmail.com>
2022-05-06 17:09:14 +0100

    cart item db methods added

--- core/repositories/cart/cart.go
+++ core/repositories/cart/cart.go
@@ -1,6 +1,9 @@
 package cart
 
-import "gorm.io/gorm"
+import (
+	"github.com/nade-harlow/E-commerce/core/models"
+	"gorm.io/gorm"
+)
 
 type CartRepository struct {
 	DB *gorm.DB
@@ -8,4 +11,22 @@ type CartRepository struct {
 
 func New(DB *gorm.DB) *CartRepository {
 	return &CartRepository{DB}
+}
+
+func (repo *CartRepository) GetCart(userID string) ([]models.CartItem, error) {
+	var cartItems []models.CartItem
+	err := repo.DB.Where("user_id = ?", userID).Find(&cartItems).Error
+	return cartItems, err
+}
+
+func (repo CartRepository) AddItem(item models.CartItem) error {
+	return repo.DB.Create(&item).Error
+}
+
+func (repo CartRepository) RemoveItem(userID, ItemID string) error {
+	return repo.DB.Where("user_id = ? AND id = ?", userID, ItemID).Delete(&models.CartItem{}).Error
+}
+
+func (repo CartRepository) UpdateItem(userId string, itemId string, quantity int16) error {
+	return repo.DB.Model(&models.CartItem{}).Where("user_id = ? AND id = ?", userId, itemId).Update("quantity", quantity).Error
 }
