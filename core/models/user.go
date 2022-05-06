f42b5c28f6fb3806793d870174bb024f024a768e
nade-harlow <omonadefranklyn@gmail.com>
2022-05-06 14:08:08 +0100

    validation tags added to user struct

--- core/models/user.go
+++ core/models/user.go
@@ -7,29 +7,29 @@ import (
 
 type User struct {
 	Model
-	FirstName  string         `json:"first_name"`
-	LastName   string         `json:"last_name"`
-	Username   string         `gorm:"unique" json:"username"`
-	Role       string         `json:"role,omitempty"`
-	Email      string         `gorm:"unique" json:"email"`
-	Password   string         `json:"password,omitempty"`
-	Telephone  string         `json:"telephone"`
-	IsVerified bool           `json:"is_verified"`
-	CreatedAt  time.Time      `json:"created_at"`
-	UpdatedAt  time.Time      `json:"updated_at"`
-	DeletedAt  gorm.DeletedAt `gorm:"index" json:"-" `
+	FirstName       string         `json:"first_name" validate:"required"`
+	LastName        string         `json:"last_name"`
+	Username        string         `gorm:"unique" json:"username"`
+	Role            string         `json:"role,omitempty"`
+	Email           string         `gorm:"unique" json:"email" validate:"required,email"`
+	Password        string         `json:"password,omitempty" validate:"required,min=8,eqfield=ConfirmPassword"`
+	ConfirmPassword string         `json:"confirm_password,omitempty" gorm:"-" validate:"required,min=8,eqfield=Password"`
+	Telephone       string         `json:"telephone" validate:"required"`
+	IsVerified      bool           `json:"is_verified"`
+	CreatedAt       time.Time      `json:"created_at"`
+	UpdatedAt       time.Time      `json:"updated_at"`
+	DeletedAt       gorm.DeletedAt `gorm:"index" json:"-" `
 }
 
 type UserAddress struct {
 	Model
 	UserID       string         `json:"user_id"`
 	User         User           `json:"-"`
-	AddressLine1 string         `json:"address_line_1"`
+	AddressLine1 string         `json:"address_line_1" validate:"required"`
 	AddressLine2 string         `json:"address_line_2"`
-	City         string         `json:"city"`
-	PostalCode   string         `json:"postal_code"`
-	Country      string         `json:"country"`
-	Telephone    string         `json:"telephone"`
+	City         string         `json:"city" validate:"required"`
+	PostalCode   string         `json:"postal_code" validate:"required"`
+	Country      string         `json:"country" validate:"required"`
 	Mobile       string         `json:"mobile"`
 	CreatedAt    time.Time      `json:"created_at"`
 	UpdatedAt    time.Time      `json:"updated_at"`
