6e5d3059e2bcdf9d39ff1f4076152b3a15b8bb85
nade-harlow <omonadefranklyn@gmail.com>
2022-05-06 15:12:07 +0100

    added cart repository

---
+++ ports/repositories/cart.go
@@ -0,0 +1,8 @@
+package repositories
+
+type CartRepository interface {
+	//GetCart(userId string) ([]CartItem, error)
+	//AddItem(userId string, item CartItem) error
+	//RemoveItem(userId string, itemId string) error
+	//UpdateItem(userId string, itemId string, quantity int) error
+}
