1f669d22413f7d832f496b19c61cf6d9d0443437
nade-harlow <omonadefranklyn@gmail.com>
2022-05-06 14:53:14 +0100

    added get product to product repository interface

--- ports/repositories/product.go
+++ ports/repositories/product.go
@@ -4,6 +4,7 @@ import "github.com/nade-harlow/E-commerce/core/models"
 
 type ProductRepository interface {
 	CreateProduct(product *models.Product) error
+	GetProduct(productID string) (*models.Product, error)
 	GetAllProducts() ([]models.Product, error)
 	UpdateProduct(productID string, product map[string]interface{}) error
 	DeleteProduct(productID string) error
