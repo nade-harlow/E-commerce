4df2489c2f582880390fd15f4fc7d5e5ccdb2bec
nade-harlow <omonadefranklyn@gmail.com>
2022-05-06 17:10:19 +0100

    cart service implemented

--- ports/services/cart.go
+++ ports/services/cart.go
@@ -1,10 +1,18 @@
 package services
 
 import (
+	"errors"
+	"github.com/nade-harlow/E-commerce/adapter/repository/database/redisql"
+	"github.com/nade-harlow/E-commerce/core/models"
 	repository2 "github.com/nade-harlow/E-commerce/ports/repositories"
+	"log"
 )
 
 type CartServices interface {
+	GetCart() ([]models.CartItem, error)
+	AddItem(productID string) error
+	RemoveItem(ItemID string) error
+	UpdateItem(itemId string, quantity int16) error
 }
 
 type CartService struct {
@@ -15,4 +23,43 @@ func NewCartService(repository repository2.CartRepository) *CartService {
 	return &CartService{
 		repository: repository,
 	}
+}
+
+func (cart *CartService) GetCart() ([]models.CartItem, error) {
+	ok, value := redisql.ValidateRedisKey("userID")
+	if ok {
+		return cart.repository.GetCart(value.(string))
+	}
+	return nil, errors.New("userID not found: session expired")
+}
+
+func (cart *CartService) AddItem(productID string) error {
+	log.Println("productID: ", productID)
+	ok, value := redisql.ValidateRedisKey("userID")
+	item := models.CartItem{
+		UserID:    value.(string),
+		ProductID: productID,
+		Quantity:  1,
+	}
+	if ok {
+		return cart.repository.AddItem(item)
+	}
+	return errors.New("userID not found: session expired")
+}
+
+func (cart *CartService) RemoveItem(ItemID string) error {
+	ok, value := redisql.ValidateRedisKey("userID")
+	if ok {
+		return cart.repository.RemoveItem(value.(string), ItemID)
+	}
+	return errors.New("userID not found: session expired")
+}
+
+func (cart *CartService) UpdateItem(itemId string, quantity int16) error {
+	ok, value := redisql.ValidateRedisKey("userID")
+	if ok {
+		return cart.repository.UpdateItem(value.(string), itemId, quantity)
+	}
+	return errors.New("userID not found: session expired")
+
 }
