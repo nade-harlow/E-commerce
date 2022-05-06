41de2d04cf7b5f2fb105ea6b9c11a4bc67171bef
nade-harlow <omonadefranklyn@gmail.com>
2022-05-06 15:33:37 +0100

    get cart handler added

--- adapter/api/controllers/cart/cart.go
+++ adapter/api/controllers/cart/cart.go
@@ -2,6 +2,7 @@ package cart
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/nade-harlow/E-commerce/core/utils/response"
 	"github.com/nade-harlow/E-commerce/ports/services"
 )
 
@@ -13,5 +14,17 @@ type CartController struct {
 func NewCartController(productService services.CartServices) *CartController {
 	return &CartController{
 		CartService: productService,
+	}
+}
+
+func (cart CartController) GetItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		itemID := c.Param("id")
+		item, err := cart.CartService.GetCart(itemID)
+		if err != nil {
+			response.Json(c, 500, "error fetching cart", nil, err.Error())
+			return
+		}
+		response.Json(c, 200, "cart fetched", item, nil)
 	}
 }
