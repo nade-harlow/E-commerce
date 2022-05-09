2104051e9bda3e8cd14701f6257a25018229ee89
nade-harlow <omonadefranklyn@gmail.com>
2022-05-09 12:59:01 +0100

    request models refactored

--- core/requests/request.go
+++ core/requests/request.go
@@ -1,17 +1,26 @@
 package requests
 
-type UserRequest struct {
-	FirstName   string `json:"first_name"`
-	LastName    string `json:"last_name"`
-	Username    string `json:"username"`
-	Country     string `json:"country"`
-	DateOfBirth string `json:"dateofbirth"`
-	Avatar      string `json:"avatar"`
-	Email       string `gorm:"unique" json:"email"`
-	Password    string `json:"password,omitempty"`
+type UserSignUpRequest struct {
+	FirstName       string `json:"first_name" validate:"required"`
+	LastName        string `json:"last_name"`
+	Role            string `json:"role,omitempty"`
+	Email           string `gorm:"unique" json:"email" validate:"required,email"`
+	Password        string `json:"password,omitempty" validate:"required,min=8,eqfield=ConfirmPassword"`
+	ConfirmPassword string `json:"confirm_password,omitempty" gorm:"-" validate:"required,min=8,eqfield=Password"`
+	Telephone       string `json:"telephone" validate:"required"`
 }
 
 type UserLoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type UserAddressRequest struct {
+	UserID       string `json:"user_id"`
+	AddressLine1 string `json:"address_line_1" validate:"required"`
+	AddressLine2 string `json:"address_line_2"`
+	City         string `json:"city" validate:"required"`
+	PostalCode   string `json:"postal_code" validate:"required"`
+	Country      string `json:"country" validate:"required"`
+	Mobile       string `json:"mobile"`
 }
